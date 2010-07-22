187b0deee0013eff2af807eafd57f7a29ce57326
手嶋守 <tejima@gmail.com>
2010-07-23 00:58:03 +0900

    first commit.

---
+++ apps/pc_frontend/modules/calendar/templates/_calendarBoxFriend.php
@@ -0,0 +1,4 @@
+<iframe src="https://www.google.com/calendar/hosted/<?php echo $domain; ?>/embed?showTitle=0&showPrint=0&showTabs=0&showCalendars=0&showTz=0&mode=WEEK&height=600&wkst=2&bgcolor=%23FFFFFF&src=<?php echo urlencode($mail); ?>&color=%23853104&ctz=Asia%2FTokyo"
+style=" border-width:0 " width="715" height="600" frameborder="0"
+scrolling="no"></iframe>
+
